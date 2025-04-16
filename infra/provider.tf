0575b963b6d724480062228788e0ee25e7b090b6
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-04-16 10:06:42 +0530

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,10 +5,16 @@ terraform {
       version = "3.102.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "transinfra"
+    storage_account_name = "infrastoragetfstate1"
+    container_name       = "infra-tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  features {
-  }
-subscription_id = "66b3f5b6-8e0e-40da-bb11-2e607df4cf26"
+  features {}
+  subscription_id = var.subscription_id
 }
