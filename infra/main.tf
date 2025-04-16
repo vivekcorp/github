80d9b7671d8a2921b29240c2c86b32b6580497fb
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-04-16 07:17:55 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,10 +1,28 @@
 module "resource_group_name" {
     for_each = var.vivekRG
     source   = "./RG"
-    vivekRG = var.vivekRG
+    name     = each.value.name
+    location = each.value.location
+  
 }
 module "nic" {
   for_each = var.networkin
+  depends_on = [ module.Vnet ]
   source   = "./nic"
-  networkin =var.networkin
+    networkin = var.networkin
+
+}
+module "nsg" {
+  for_each = var.nsggroup
+  depends_on = [ module.resource_group_name ]
+  source   = "./nsg"
+  nsggroup = var.nsggroup
+  
+}
+module "Vnet" {
+    for_each = var.VMVnet
+    depends_on = [ module.resource_group_name ]
+    source   = "./Vnet"
+    VMVnet = var.VMVnet
+  
 }
