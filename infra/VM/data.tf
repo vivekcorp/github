684230db588c513d0839caf58596884cfdde1f1f
vivek <vivekcor@yahoo.com>
2025-04-16 08:42:30 +0530

    add

--- infra/VM/data.tf
+++ infra/VM/data.tf
@@ -1,42 +1,25 @@
-resource "azurerm_managed_disk" "datadisk" {
-  for_each              = var.vmsetup
-  name                  = "${each.value.virtual_machine_name}-datadisk"
-  resource_group_name   = each.value.resource_group_name
-  location              = each.value.location
-  storage_account_type  = "Standard_LRS"
-  create_option         = "Empty"
-  disk_size_gb          = 128
-}
-
-resource "azurerm_windows_virtual_machine" "vmcode" {
+data "azurerm_key_vault_secret" "keyvaultuser" {
   for_each = var.vmsetup
-  name                = each.value.virtual_machine_name
-  resource_group_name = each.value.resource_group_name
-  location            = each.value.location
-  size                = "Standard_F2"
-  admin_username      = data.azurerm_key_vault_secret.keyvaultuser[each.key].value
-  admin_password      = data.azurerm_key_vault_secret.keyvaultpass[each.key].value
-  network_interface_ids = [data.azurerm_network_interface.datanic[each.key].id]
+   name         = "username"
+  key_vault_id = data.azurerm_key_vault.keyvault[each.key].id
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-
-  }
-  
-  resource "azurerm_virtual_machine_data_disk_attachment" "datadisk_attachment" {
-    for_each            = var.vmsetup
-    managed_disk_id     = azurerm_managed_disk.datadisk[each.key].id
-    virtual_machine_id  = azurerm_windows_virtual_machine.vmcode[each.key].id
-    lun                 = 0
-    caching             = "ReadWrite"
 }
 
+data "azurerm_key_vault_secret" "keyvaultpass" {
+  for_each = var.vmsetup
+  name         = "passwordadmin"
+  key_vault_id = data.azurerm_key_vault.keyvault[each.key].id
+
+}
+
+data "azurerm_key_vault" "keyvault" {
+  for_each = var.vmsetup
+  name                = each.value.keyval_name
+  resource_group_name = each.value.resource_group_name
+}
+
+data "azurerm_network_interface" "datanic" {
+  for_each = var.vmsetup
+  name                = each.value.network_interface_name
+  resource_group_name = each.value.resource_group_name
+}
--- infra/VM/main.tf
+++ infra/VM/main.tf
@@ -1,3 +1,13 @@
+resource "azurerm_managed_disk" "datadisk" {
+  for_each              = var.vmsetup
+  name                  = "${each.value.virtual_machine_name}-datadisk"
+  resource_group_name   = each.value.resource_group_name
+  location              = each.value.location
+  storage_account_type  = "Standard_LRS"
+  create_option         = "Empty"
+  disk_size_gb          = 128
+}
+
 resource "azurerm_windows_virtual_machine" "vmcode" {
   for_each = var.vmsetup
   name                = each.value.virtual_machine_name
@@ -6,8 +16,7 @@ resource "azurerm_windows_virtual_machine" "vmcode" {
   size                = "Standard_F2"
   admin_username      = data.azurerm_key_vault_secret.keyvaultuser[each.key].value
   admin_password      = data.azurerm_key_vault_secret.keyvaultpass[each.key].value
-  network_interface_ids = [data.azurerm_network_interface.datanic[each.key].id,]
- 
+  network_interface_ids = [data.azurerm_network_interface.datanic[each.key].id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -20,21 +29,14 @@ resource "azurerm_windows_virtual_machine" "vmcode" {
     sku       = "2016-Datacenter"
     version   = "latest"
   }
-}
 
-resource "azurerm_virtual_machine_extension" "vm_extension_install_iis" {
-  for_each = var.vmsetup
-  name                       = "vm_extension_install_iis"
-  virtual_machine_id         = azurerm_windows_virtual_machine.vmcode[each.key].id
-  publisher                  = "Microsoft.Compute"
-  type                       = "CustomScriptExtension"
-  type_handler_version       = "1.8"
-  auto_upgrade_minor_version = true
-
-  settings = <<SETTINGS
-    {
-        "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
-    }
-SETTINGS
+  }
+  
+  resource "azurerm_virtual_machine_data_disk_attachment" "datadisk_attachment" {
+    for_each            = var.vmsetup
+    managed_disk_id     = azurerm_managed_disk.datadisk[each.key].id
+    virtual_machine_id  = azurerm_windows_virtual_machine.vmcode[each.key].id
+    lun                 = 0
+    caching             = "ReadWrite"
 }
 
