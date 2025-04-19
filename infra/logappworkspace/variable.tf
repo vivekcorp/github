c2cbf8d8236420579d82f217f7b3c6961db463f3
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-04-19 10:37:57 +0530

    Update variable.tf

--- infra/logappworkspace/variable.tf
+++ infra/logappworkspace/variable.tf
@@ -1,5 +1,15 @@
-variable "workspace_name" {}
-variable "location" {}
 variable "resource_group_name" {}
-variable "sku" {}
-variable "retention_in_days" {}
+variable "location" {}
+
+variable "workspace_name" {}
+variable "la_sku" {
+  default = "PerGB2018"
+}
+variable "retention_in_days" {
+  default = 30
+}
+
+variable "app_insights_name" {}
+variable "application_type" {
+  default = "web"
+}
