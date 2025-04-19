58130da064d40da250a1517fe197dd3ae8e2673b
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-04-19 10:38:26 +0530

    Update output.tf

--- infra/logappworkspace/output.tf
+++ infra/logappworkspace/output.tf
@@ -1,3 +1,7 @@
-output "workspace_id" {
-  value = azurerm_log_analytics_workspace.this.id
+output "log_analytics_workspace_id" {
+  value = azurerm_log_analytics_workspace.log_analytics.id
+}
+
+output "app_insights_instrumentation_key" {
+  value = azurerm_application_insights.app_insights.instrumentation_key
 }
