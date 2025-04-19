19729ae2e748eed5e441773330a2af94ae39b21b
vivekcorp <95239073+vivekcorp@users.noreply.github.com>
2025-04-19 10:37:06 +0530

    Update main.tf

--- infra/logappworkspace/main.tf
+++ infra/logappworkspace/main.tf
@@ -1,7 +1,15 @@
-resource "azurerm_log_analytics_workspace" "this" {
+resource "azurerm_log_analytics_workspace" "log_analytics" {
   name                = var.workspace_name
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = var.sku
+  sku                 = var.la_sku
   retention_in_days   = var.retention_in_days
+}
+
+resource "azurerm_application_insights" "app_insights" {
+  name                = var.app_insights_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  application_type    = var.application_type
+  workspace_id        = azurerm_log_analytics_workspace.log_analytics.id
 }
